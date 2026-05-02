d637d8baebd6e135daf169a6ad4c3c98b5715f36
5255umutcan33 <5255umutcan33@gmail.com>
2026-05-03 02:05:42 +0300

    l

--- commands/verify2.js
+++ commands/verify2.js
@@ -3,15 +3,10 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommand
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('verify2')
-        .setDescription('Doğrulama panelini kurar.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator), // Sadece adminler açabilir
+        .setDescription('LUAWARE Dil Seçimli Doğrulama Paneli Kurar.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
     async execute(interaction) {
-        // --- AYARLAR ---
-        const verifyChannelId = "1500266130495897722";
-        const trRoleId = "1500268780037607544"; // Discord'dan Turkish rolünün ID'sini al
-        const enRoleId = "1500268646545756392"; // Discord'dan English rolünün ID'sini al
-
         const embed = new EmbedBuilder()
             .setTitle("LUAWARE | VERIFICATION")
             .setDescription("Lütfen dilinizi seçerek sunucuya giriş yapın.\nPlease select your language to enter the server.")
@@ -32,7 +27,7 @@ module.exports = {
                     .setStyle(ButtonStyle.Secondary),
             );
 
-        await interaction.reply({ content: 'Doğrulama paneli kuruluyor...', ephemeral: true });
+        await interaction.reply({ content: 'Doğrulama paneli başarıyla oluşturuldu.', ephemeral: true });
         await interaction.channel.send({ embeds: [embed], components: [row] });
     },
 };
