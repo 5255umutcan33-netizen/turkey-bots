30da83efc418f18ac397f337d68f5f8f4cd6172c
5255umutcan33 <5255umutcan33@gmail.com>
2026-05-11 23:52:06 +0300

    l

--- commands/partnerekle.js
+++ commands/partnerekle.js
@@ -1,4 +1,4 @@
--const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 const PartnerModel = require('../models/Partner'); // Şemanın yolunu kendine göre ayarla
 
 module.exports = {
